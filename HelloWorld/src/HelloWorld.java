f4b32812a0a2c4a2c9dc7fc274e33a9b49b7b4fe
Laila Alhmoud <tflm1014@gmail.com>
2016-03-28 23:07:51 -0400

    this is ver 2

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,7 +3,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Version 1");
+System.out.println("Version 2");
 	}
-
 }
